c29bc5ab7fa4508639c2b00479a583057aa044f1
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-10-24 19:30:06 +0200

    Add Loading and Error

---
+++ src/common/Error.tsx
@@ -0,0 +1,3 @@
+import React, { ReactElement } from 'react';
+
+export const Error = (): ReactElement => <div className="error">An error occurred during fetch.</div>;
---
+++ src/common/Loading.tsx
@@ -0,0 +1,3 @@
+import React, { ReactElement } from 'react';
+
+export const Loading = (): ReactElement => <div>Loading...</div>;
